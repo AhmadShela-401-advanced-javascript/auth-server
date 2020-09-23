e396369aa243aa7ebbaf3747b11b15bd5b776316
ahmed <ahmedsh717@gmail.com>
2020-09-24 02:03:13 +0300

    class14

---
+++ src/auth/middleware/acl-middleware.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = (action)=>{
+    return (req,res,next)=>{
+        console.log('Hiiiiiii i am innnnn',req.user);
+        if(req.user){
+            if(req.user.role.includes(action)){
+               return next();
+            }
+            return  next('You dont have permission to this rout !!');
+        }
+    }
+}
