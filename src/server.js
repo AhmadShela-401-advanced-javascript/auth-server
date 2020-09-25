f822184ec6f6b8281f3f7dbe5cdc2524c8d395be
Ahmad Shela <ahmedsh717@gmail.com>
2020-09-25 20:49:54 +0300

    class12B

--- src/server.js
+++ src/server.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
+app.use(express.static('./public')); 
 app.use(express.json());
+
 app.use(router)
 app.use(cors());
 app.use(morgan('dev'));
